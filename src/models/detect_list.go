27429940f628cad8d45f893f0a2f35daf1abdfbc
0x1f86dude <razercloud@tutanota.com>
2019-08-13 00:23:28 +0800

    fix gorm tags not working

--- src/models/detect_list.go
+++ src/models/detect_list.go
@@ -1,13 +1,13 @@
 package models
 
 type DetectList struct {
-  Id        	int64     	`json:"id";gorm:"column:id;NOT NULL;PRIMARY KEY;"`
-  Name      	string    	`json:"name";gorm:"column:name;NOT NULL;type:longtext;"`
-  Text      	string    	`json:"text";gorm:"column:text;NOT NULL;type:longtext;"`
-  Regex     	string    	`json:"regex";gorm:"column:regex;NOT NULL;type:longtext;"`
-  Type      	int       	`json:"type";gorm:"column:type;NOT NULL;"`
+	Id    int64  `gorm:"column:id;NOT NULL;PRIMARY KEY;" json:"id";`
+	Name  string `gorm:"column:name;NOT NULL;type:longtext;" json:"name";`
+	Text  string `gorm:"column:text;NOT NULL;type:longtext;" json:"text";`
+	Regex string `gorm:"column:regex;NOT NULL;type:longtext;" json:"regex";`
+	Type  int    `gorm:"column:type;NOT NULL;" json:"type";`
 }
 
 func (DetectList) TableName() string {
-  return "detect_list"
+	return "detect_list"
 }
--- src/models/speedtest.go
+++ src/models/speedtest.go
@@ -1,20 +1,20 @@
 package models
 
 type SpeedTest struct {
-  Id        	    int64     	`json:"id";gorm:"column:id;NOT NULL;PRIMARY KEY;"`
-  NodeId    	    int       	`json:"nodeid";gorm:"column:nodeid;NOT NULL;"`
-  DateTime  	    int64     	`json:"datetime";gorm:"column:datetime;NOT NULL;"`
-  TelecomePing	    string    	`json:"telecomping";gorm:"column:telecomping;NOT NULL;type:text;"`
-  TelecomeUpload	string    	`json:"telecomeupload";gorm:"column:telecomeupload;NOT NULL;type:text;"`
-  TelecomeDownload	string    	`json:"telecomedownload";gorm:"column:telecomedownload;NOT NULL;type:text;"`
-  UnicomPing	    string    	`json:"unicomping";gorm:"column:unicomping;NOT NULL;type:text;"`
-  UnicomUpload	    string    	`json:"unicomupload";gorm:"column:unicomupload;NOT NULL;type:text;"`
-  UnicomDownload	string    	`json:"unicomdownload";gorm:"column:unicomdownload;NOT NULL;type:text;"`
-  CmccPing  	    string    	`json:"cmccping";gorm:"column:cmccping;NOT NULL;type:text;"`
-  CmccUpload	    string    	`json:"cmccupload";gorm:"column:cmccupload;NOT NULL;type:text;"`
-  CmccDownload	    string    	`json:"cmccdownload";gorm:"column:cmccdownload;NOT NULL;type:text;"`
+	Id               int64  `gorm:"column:id;NOT NULL;PRIMARY KEY;" json:"id";`
+	NodeId           int    `gorm:"column:nodeid;NOT NULL;" json:"nodeid"`
+	DateTime         int64  `gorm:"column:datetime;NOT NULL;" json:"datetime"`
+	TelecomePing     string `gorm:"column:telecomping;NOT NULL;type:text;" json:"telecomping";`
+	TelecomeUpload   string `gorm:"column:telecomeupload;NOT NULL;type:text;" json:"telecomeupload";`
+	TelecomeDownload string `gorm:"column:telecomedownload;NOT NULL;type:text;" json:"telecomedownload";`
+	UnicomPing       string `gorm:"column:unicomping;NOT NULL;type:text;" json:"unicomping";`
+	UnicomUpload     string `gorm:"column:unicomupload;NOT NULL;type:text;" json:"unicomupload";`
+	UnicomDownload   string `gorm:"column:unicomdownload;NOT NULL;type:text;" json:"unicomdownload";`
+	CmccPing         string `gorm:"column:cmccping;NOT NULL;type:text;" json:"cmccping";`
+	CmccUpload       string `gorm:"column:cmccupload;NOT NULL;type:text;" json:"cmccupload";`
+	CmccDownload     string `gorm:"column:cmccdownload;NOT NULL;type:text;" json:"cmccdownload";`
 }
 
 func (SpeedTest) TableName() string {
-  return "speedtest"
+	return "speedtest"
 }
